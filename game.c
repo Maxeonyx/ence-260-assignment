b6fc93b95db2a66a87a7f473043fc489ab043b60
Maxwell Clarke <maxeonyx@gmail.com>
2016-10-05 17:03:41 +1300

    Display text on the screen

--- game.c
+++ game.c
@@ -1,14 +1,29 @@
 #include "system.h"
+#include "tinygl.h"
+#include "task.h"
+#include "../../fonts/font5x7_1.h"
+
+static void display_task (__unused__ void *data) {
+    tinygl_update ();
+}
+
 
 int main (void)
 {
     system_init ();
 
+    tinygl_init (250);
+    tinygl_font_set (&font5x7_1);
+    tinygl_text_speed_set (2);
+    tinygl_text_mode_set (TINYGL_TEXT_MODE_SCROLL);
 
-    while (1)
+    tinygl_text ("hi world");
+
+    task_t tasks[] =
     {
+      {.func = display_task, .period = TASK_RATE / 250}
+    };
 
+    task_schedule (tasks, ARRAY_SIZE (tasks));
 
-
-    }
 }
