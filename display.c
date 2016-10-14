9b28b7436152971b598e7695d32cd846c0379042
maxeonyx <maxeonyx@gmail.com>
2016-10-14 16:07:30 +1300

    WIP refactor

--- display.c
+++ display.c
@@ -15,7 +15,7 @@ void set_scroll_text (void) {
     tinygl_text_mode_set (TINYGL_TEXT_MODE_SCROLL);
 }
 
-void set_scroll_text (void) {
+void set_static_text (void) {
     tinygl_text_speed_set (100);
     tinygl_text_dir_set (TINYGL_TEXT_DIR_ROTATE);
     tinygl_text_mode_set (TINYGL_TEXT_MODE_STEP);
